96be4c118d3e1fc0c22d0b880a275d3b1495254b
Juhani Kauppi <jkauppi@student.hive.fi>
2019-11-01 12:02:20 +0200

    Read more than 1 input files simultaneously

--- get_next_line_test.c
+++ get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 10:54:32 by jkauppi           #+#    #+#             */
-/*   Updated: 2019/10/31 17:52:52 by jkauppi          ###   ########.fr       */
+/*   Updated: 2019/11/01 12:01:03 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,42 @@
 
 int			main(int argc, char **argv)
 {
-	char	**line;
-	size_t	line_num;
-	int		fd;
+	char		**line;
+	size_t		line_num;
+	int			index;
+	int			fd_array[FD_SIZE];
+	char		**line_buffer[FD_SIZE];
+	int			are_lines;
 
-	if (argc == 2)
+	if (argc > 1)
 	{
-		fd = open(argv[1], O_RDONLY);
-		line_num = 0;
-		line = (char **)ft_memalloc(sizeof(*line));
-		while ((get_next_line(fd, line) > 0))
+		index = 0;
+		ft_memset(fd_array, 0, FD_SIZE);
+		while (++index < argc)
 		{
-			ft_putnbr(++line_num);
-			ft_putstr(": ");
-			ft_putendl(*line);
+			fd_array[index - 1] = open(argv[index], O_RDONLY);
+			line = (char **)ft_memalloc(sizeof(*line));
+			line_buffer[index - 1] = (char **)ft_memalloc(sizeof(**line_buffer));
+		}
+		line_num = 0;
+		are_lines = 1;
+		while (are_lines)
+		{
+			are_lines = 0;
+			index = 0;
+			while (index < FD_SIZE && fd_array[index])
+			{
+				if ((get_next_line(fd_array[index], line_buffer[index]) > 0))
+				{
+					ft_putnbr(++line_num);
+					ft_putstr(": ");
+					ft_putendl(*line_buffer[index]);
+					are_lines = 1;
+				}
+				else
+					fd_array[index] = 0;
+				index++;
+			}
 		}
 	}
 	return (0);
