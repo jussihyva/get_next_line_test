29e3f8bcbf4a5ea5c6ab871514a29150dec711ad
Juhani Kauppi <jkauppi@student.hive.fi>
2019-11-01 14:29:32 +0200

    Main read characters from commandline but not from pipe.

--- get_next_line_test.c
+++ get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 10:54:32 by jkauppi           #+#    #+#             */
-/*   Updated: 2019/11/01 13:20:30 by jkauppi          ###   ########.fr       */
+/*   Updated: 2019/11/01 14:27:22 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,43 @@ int			main(int argc, char **argv)
 	char		**line_buffer[FD_SIZE];
 	int			are_lines;
 
+	ft_memset(fd_array, 0, FD_SIZE);
 	if (argc > 1)
 	{
 		index = 0;
-		ft_memset(fd_array, 0, FD_SIZE);
 		while (++index < argc)
 		{
 			fd_array[index - 1] = open(argv[index], O_RDONLY);
 			line = (char **)ft_memalloc(sizeof(*line));
 			line_buffer[index - 1] = (char **)ft_memalloc(sizeof(**line_buffer));
 		}
-		line_num = 0;
-		are_lines = 1;
-		while (are_lines)
+	}
+	else
+	{
+		index = 1;
+		fd_array[index - 1] = 1;
+		line = (char **)ft_memalloc(sizeof(*line));
+		line_buffer[index - 1] = (char **)ft_memalloc(sizeof(**line_buffer));
+	}
+	line_num = 0;
+	are_lines = 1;
+	while (are_lines)
+	{
+		are_lines = 0;
+		index = 0;
+		line_num++;
+		while (index < FD_SIZE && fd_array[index] && fd_array[index] >= 0)
 		{
-			are_lines = 0;
-			index = 0;
-			line_num++;
-			while (index < FD_SIZE && fd_array[index])
+			if ((get_next_line(fd_array[index], line_buffer[index]) > 0))
 			{
-				if ((get_next_line(fd_array[index], line_buffer[index]) > 0))
-				{
-					ft_putnbr(line_num);
-					ft_putstr(": ");
-					ft_putendl(*line_buffer[index]);
-					are_lines = 1;
-				}
-				else
-					fd_array[index] = 0;
-				index++;
+				ft_putnbr(line_num);
+				ft_putstr(": ");
+				ft_putendl(*line_buffer[index]);
+				are_lines = 1;
 			}
+			else
+				fd_array[index] = 0;
+			index++;
 		}
 	}
 	return (0);
